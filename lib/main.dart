22ba6938055e2ddd16d4aa90fd70d761fdeb3096
ojeanne <oj.jeanne@gmail.com>
2024-03-26 15:24:55 -0400

    Nettoyage du code pour base.

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'ASLC2',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+    //home: ListeLots(LotViewModel(RemoteData())),
+    //home: TresoView(TresoViewModel(RemoteData())));
+  }
+}
+
+
+/*
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -123,3 +143,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+*/
